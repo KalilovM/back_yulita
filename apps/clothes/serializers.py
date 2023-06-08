d0d7ebcc1471442aaadde84c892096d984827802
KalilovM <marlenkalilov86@gmail.com>
2023-06-08 17:14:16 +0200

    add create image urls, views and serializers

--- apps/clothes/serializers.py
+++ apps/clothes/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 
-from .models import Cloth, ClothType, Suit
+from .models import Cloth, ClothType, Suit, ClothSampleImage, ClothModelImage
 
 
 class ClothSerializer(serializers.ModelSerializer):
+    model_images = serializers.SerializerMethodField()
+    sample_images = serializers.SerializerMethodField()
     class Meta:
         model = Cloth
         fields = '__all__'
+        
+    def get_model_images(self, obj):
+        request = self.context.get('request')
+        images = obj.model_images.all()
+        if images:
+            return [{'id': image.id, 'image': request.build_absolute_uri(image.image.url)} for image in images]
+        
+    def get_sample_images(self, obj):
+        request = self.context.get('request')
+        images = obj.sample_images.all()
+        if images:
+            return [{'id': image.id, 'image': request.build_absolute_uri(image.image.url)} for image in images]
 
 
 class ClothListSerializer(serializers.ModelSerializer):
@@ -36,4 +50,16 @@ class ClothTypeSerializer(serializers.ModelSerializer):
 class SuitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Suit
+        fields = '__all__'
+        
+        
+class ClothSampleImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClothSampleImage
+        fields = '__all__'
+        
+        
+class ClothModelImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClothModelImage
         fields = '__all__'
